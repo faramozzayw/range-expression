961f39f49868234ba01b7dbd0852607637d6adfa
faramozzayw <faramo.zayw@gmail.com>
2021-01-27 18:10:24 +0200

    [wip]

---
+++ __test__/range.spec.ts
@@ -0,0 +1,127 @@
+import {
+	RangeExpr,
+	RangeFromExpr,
+	RangeToExpr,
+	RangeFullExpr,
+	RangeInclusiveExpr,
+	RangeToInclusiveExpr,
+} from "../src/index";
+
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const str = "get correct slice";
+
+describe("ranges", () => {
+	it("iterating over the range is correct", () => {
+		let sum = 0;
+		for (const i of new RangeExpr(0, 4, true)) {
+			sum += i;
+		}
+
+		expect(sum).toEqual(10);
+	});
+
+	it("get correct slice when using RangeExpr from array", () => {
+		const args1 = new RangeExpr(5, 7, true).getBounds();
+
+		expect(array.slice(...args1)).toEqual([5, 6, 7]);
+
+		const args2 = new RangeExpr(5, 7).getBounds();
+
+		expect(array.slice(...args2)).toEqual([5, 6]);
+	});
+
+	it("get correct slice when using RangeExpr from string", () => {
+		const args = new RangeExpr(1, 7, true).getBounds();
+
+		expect(str.slice(...args)).toEqual("et corr");
+	});
+
+	it("get correct slice when using RangeFromExpr from array", () => {
+		const args = new RangeFromExpr(5).getBounds();
+
+		expect(array.slice(...args)).toEqual([5, 6, 7, 8, 9, 10]);
+	});
+
+	it("get correct slice when using RangeFromExpr from string", () => {
+		const args = new RangeFromExpr(1).getBounds();
+
+		expect(str.slice(...args)).toEqual("et correct slice");
+	});
+
+	it("get correct slice when using RangeToExpr from array", () => {
+		const args = new RangeToExpr(5).getBounds();
+
+		expect(array.slice(...args)).toEqual([0, 1, 2, 3, 4]);
+	});
+
+	it("get correct slice when using RangeToExpr from string", () => {
+		const args = new RangeToExpr(5).getBounds();
+
+		expect(str.slice(...args)).toEqual("get c");
+	});
+
+	it("get correct slice when using RangeFullExpr from array", () => {
+		const args = new RangeFullExpr().getBounds();
+
+		expect(array.slice(...args)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+	});
+
+	it("get correct slice when using RangeFullExpr from string", () => {
+		const args = new RangeFullExpr().getBounds();
+
+		expect(str.slice(...args)).toEqual("get correct slice");
+	});
+
+	it("get correct slice when using RangeInclusiveExpr from array", () => {
+		const args = new RangeInclusiveExpr(0, 5).getBounds();
+
+		expect(array.slice(...args)).toEqual([0, 1, 2, 3, 4, 5]);
+	});
+
+	it("get correct slice when using RangeInclusiveExpr from string", () => {
+		const args = new RangeInclusiveExpr(0, 5).getBounds();
+
+		expect(str.slice(...args)).toEqual("get co");
+	});
+
+	it("get correct slice when using RangeToInclusiveExpr from array", () => {
+		const args = new RangeToInclusiveExpr(5).getBounds();
+
+		expect(array.slice(...args)).toEqual([0, 1, 2, 3, 4, 5]);
+	});
+
+	it("get correct slice when using RangeToInclusiveExpr from string", () => {
+		const args = new RangeToInclusiveExpr(5).getBounds();
+
+		expect(str.slice(...args)).toEqual("get co");
+	});
+
+	it("`isEmpty` works correctly", () => {
+		expect(new RangeExpr(3, 3).isEmpty()).toBeTruthy();
+		expect(new RangeExpr(1, 2).isEmpty()).toBeFalsy();
+		expect(new RangeExpr(2, 2, true).isEmpty()).toBeFalsy();
+
+		expect(new RangeFromExpr(5).isEmpty()).toBeFalsy();
+		expect(new RangeFromExpr(Infinity).isEmpty()).toBeTruthy();
+
+		expect(new RangeToExpr(5).isEmpty()).toBeFalsy();
+		expect(new RangeToExpr(-Infinity).isEmpty()).toBeTruthy();
+
+		expect(new RangeFullExpr().isEmpty()).toBeFalsy();
+
+		expect(new RangeInclusiveExpr(1, 2).isEmpty()).toBeFalsy();
+		expect(new RangeInclusiveExpr(2, 2).isEmpty()).toBeFalsy();
+		expect(new RangeInclusiveExpr(3, 2).isEmpty()).toBeTruthy();
+
+		expect(new RangeToInclusiveExpr(5).isEmpty()).toBeFalsy();
+		expect(new RangeToInclusiveExpr(-Infinity).isEmpty()).toBeTruthy();
+
+		expect(new RangeInclusiveExpr(2, 4).isEmpty()).toBeFalsy();
+		expect(new RangeInclusiveExpr(5, 4).isEmpty()).toBeTruthy();
+	});
+
+	it("toString", () => {
+		expect(new RangeExpr(1, 2).toString()).toEqual("1..2");
+		expect(new RangeExpr(1, 2, true).toString()).toEqual("1..=2");
+	});
+});
--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,117 @@
 /**
  * @class RangeExpr
+ *
+ * The range `start..end` contains all values with `start <= x < end`. It is empty if `start >= end`.
  */
 export class RangeExpr {
-  /**
-   * @param start The lower bound of the range (inclusive).
-   * @param end The upper bound of the range (exclusive).
-   * @param inclusive 
-   */
-  constructor(
-    private start: number,
-    private end: number,
-    private inclusive: boolean = false
-  ) {
-    this.end = this.inclusive ? end + 1 : end;
-  }
+	public static exhaustive: boolean = false;
 
-  public getBounds(): [start: number, end: number] {
-    return [this.start, this.end];
-  }
+	private start: number;
+	private end: number;
+	private inclusive: boolean;
 
-  /**
-   * Returns `true` if `item` is contained in the range.
-   *
-   * @todo
-   */
-  public contains(item: number): boolean {
-    return false;
-  }
+	/**
+	 * @param start The lower bound of the range (inclusive).
+	 * @param end The upper bound of the range (exclusive).
+	 * @param inclusive
+	 */
+	constructor(start: number, end: number, inclusive: boolean = false) {
+		this.start = start;
+		this.end = inclusive ? end + 1 : end;
+		this.inclusive = inclusive;
+	}
 
-  /**
-   * Returns `true` if the range contains no items.
-   */
-  public isEmpty(): boolean {
-    return !(this.start < this.end);
-  }
+	public getBounds(): [start: number, end: number] {
+		return [this.start, this.end];
+	}
 
-  /**
-   * The RangeExpr.clone() method returns a RangeExpr object with boundary points identical to the cloned RangeExpr.
-   * 
-   * The returned clone is copied by **value, not reference,** so a change in either RangeExpr does not affect the other.
-   */
-  public clone(): RangeExpr {
-    return new RangeExpr(this.start, this.end);
-  }
+	/**
+	 * Returns `true` if `item` is contained in the range.
+	 *
+	 * @todo
+	 */
+	public contains(item: number): boolean {
+		return false;
+	}
 
-  *[Symbol.iterator]() {
-    const { start, end } = this;
+	/**
+	 * Returns `true` if the range contains no items.
+	 */
+	public isEmpty(): boolean {
+		return !(this.start < this.end);
+	}
 
-    for (let i = start; i < end; i++) {
-      yield i;
-    }
-  }
+	/**
+	 * The RangeExpr.clone() method returns a RangeExpr object with boundary points identical to the cloned RangeExpr.
+	 *
+	 * The returned clone is copied by **value, not reference,** so a change in either RangeExpr does not affect the other.
+	 */
+	public clone(): RangeExpr {
+		return new RangeExpr(this.start, this.end);
+	}
 
-  get [Symbol.toStringTag]() {
-    return "RangeExpr";
-  }
+	*[Symbol.iterator]() {
+		const { start, end } = this;
 
-  /**
-   * @override
-   */
-  toString() {
-    const { inclusive } = this;
-    const start = Number.isFinite(this.start) ? this.start : "";
-    const end = Number.isFinite(this.end) ? this.end : "";
+		for (let i = start; i < end; i++) {
+			yield i;
+		}
+	}
 
-    return `${start}..${end}`
-  }
+	get [Symbol.toStringTag]() {
+		return "RangeExpr";
+	}
+
+	/**
+	 * @override
+	 */
+	toString() {
+		const start = Number.isFinite(this.start) ? this.start : "";
+		const end = Number.isFinite(this.end)
+			? this.inclusive
+				? this.end - 1
+				: this.end
+			: "";
+		const inclusive = this.inclusive ? "=" : "";
+
+		return `${start}..${inclusive}${end}`;
+	}
 }
 
 export class RangeFromExpr extends RangeExpr {
-  constructor(start: number) {
-    super(start, Infinity);
-  }
+	constructor(start: number) {
+		super(start, Infinity);
+	}
+
+	[Symbol.iterator]: null = null;
 }
 
 export class RangeToExpr extends RangeExpr {
-  constructor(end: number) {
-    super(-Infinity, end);
-  }
+	constructor(end: number) {
+		super(-Infinity, end);
+	}
+
+	[Symbol.iterator]: null = null;
 }
 
 export class RangeFullExpr extends RangeExpr {
-  constructor() {
-    super(-Infinity, Infinity);
-  }
+	constructor() {
+		super(-Infinity, Infinity);
+	}
+
+	[Symbol.iterator]: null = null;
 }
 
 export class RangeInclusiveExpr extends RangeExpr {
-  constructor(start: number, end: number) {
-    super(start, end, true);
-  }
+	constructor(start: number, end: number) {
+		super(start, end, true);
+	}
 }
 
 export class RangeToInclusiveExpr extends RangeExpr {
-  constructor(end: number) {
-    super(-Infinity, end, true);
-  }
+	constructor(end: number) {
+		super(-Infinity, end, true);
+	}
+
+	[Symbol.iterator]: null = null;
 }
